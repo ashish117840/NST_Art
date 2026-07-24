3a87b216b531e9851283d9a6b718e19bb76de450
Ashish Kumar <helloashish20@gmail.com>
2026-07-24 16:29:50 +0530

    kaggle2

--- utils/utils.py
+++ utils/utils.py
@@ -2,6 +2,9 @@ from torch.utils.data import Dataset
 import os
 from PIL import Image
 from torchvision import transforms
+from PIL import ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+
 
 
 
@@ -16,14 +19,29 @@ class ImageFolderDataset(Dataset):
     def __len__(self):
         return len(self.files)
 
+    # def __getitem__(self, idx):
+    #     image_path = os.path.join(self.root, self.files[idx])
+    #     image = Image.open(image_path).convert('RGB')
+
+    #     if self.transform:
+    #         image = self.transform(image)
+
+    #     return image
+
     def __getitem__(self, idx):
         image_path = os.path.join(self.root, self.files[idx])
-        image = Image.open(image_path).convert('RGB')
 
-        if self.transform:
-            image = self.transform(image)
+        try:
+            image = Image.open(image_path).convert("RGB")
 
-        return image
+            if self.transform:
+                image = self.transform(image)
+
+            return image
+
+        except Exception as e:
+            print(f"\nERROR FILE: {image_path}")
+            raise e
     
 def get_transform(size, crop, final_size):
     transform_list = []
